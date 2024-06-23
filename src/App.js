f1b6d207eb4db02e2b74b3311dd8c5b66ce2e77a
Harshpawar333 <harshpawar333@gmail.com>
2024-06-23 12:04:15 +0530

    showing users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserForm from './components/UserForm';// Adjust the import to match the correct case
+import UserForm from './components/UserForm';
 import Showusers from './components/Showusers';
 
 function App() {
   return (
     <div className="App">
-      <UserForm /> {/* Use the component with the correct case */}
+      <UserForm /> 
       <Showusers />
     </div>
   );
